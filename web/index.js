040c470cf256ea1d9968a4962ff68f719ba396fe
nardiyansah <janatri1398@gmail.com>
2022-06-11 17:20:29 +0700

    finish action click on client

--- web/index.js
+++ web/index.js
@@ -1,3 +1,13 @@
-function mark() {
+window.addEventListener('DOMContentLoaded', () => {
+  const userMark = (tile, index) => {
+    tile.innerText = 'X';
+  }
 
-}
+  const tiles = Array.from(document.querySelectorAll('.tile'));
+
+  tiles.forEach((tile, index) => {
+    tile.addEventListener('click', () => userMark(tile, index));
+  })
+})
+
+
