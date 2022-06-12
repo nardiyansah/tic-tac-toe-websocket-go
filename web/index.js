744f0cfc50b58770ba9d5b927f9d20aad872a936
nardiyansah <janatri1398@gmail.com>
2022-06-12 13:02:09 +0700

    finish websocket flow client-server

--- web/index.js
+++ web/index.js
@@ -1,6 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
   const userMark = (tile, index) => {
     tile.innerText = 'X';
+
+    websocket = new WebSocket("ws://localhost:8080/mark")
+
+    websocket.onopen = function (event) {
+      websocket.send(index);
+    }
+
+    websocket.onmessage = function (event) {
+      idxServerMark = event.data;
+      console.log("websocket: server choices tile number ", idxServerMark)
+      tiles[idxServerMark].innerText = 'O'
+    }
   }
 
   const tiles = Array.from(document.querySelectorAll('.tile'));
@@ -8,8 +20,6 @@ window.addEventListener('DOMContentLoaded', () => {
   tiles.forEach((tile, index) => {
     tile.addEventListener('click', () => userMark(tile, index));
   })
-
-  websocket = new WebSocket("ws://localhost:8080/mark")
 })
 
 
