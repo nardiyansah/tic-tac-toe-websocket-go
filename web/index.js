c2af997c8d3e3209ac8cf02aff102544c529d14c
nardiyansah <janatri1398@gmail.com>
2022-06-12 14:29:39 +0700

    add function check winner

--- web/index.js
+++ web/index.js
@@ -1,6 +1,106 @@
 window.addEventListener('DOMContentLoaded', () => {
+  const checkWinner = (tiles) => {
+    if (
+      tiles[0].innerText === 'X' &&
+      tiles[1].innerText === 'X' &&
+      tiles[2].innerText === 'X'
+      ) {
+      if (tiles[0].innerText === 'X') {
+        alert('User win');
+      } else {
+        alert('Com win');
+      }
+    }
+
+    if (
+      tiles[3].innerText === 'X' &&
+      tiles[4].innerText === 'X' &&
+      tiles[5].innerText === 'X' 
+      ) {
+      if (tiles[3].innerText === 'X') {
+        alert('User win');
+      } else {
+        alert('Com win');
+      }
+    }
+
+    if (
+      tiles[6].innerText === 'X' &&
+      tiles[7].innerText === 'X' &&
+      tiles[8].innerText === 'X'
+      ) {
+      if (tiles[6].innerText === 'X') {
+        alert('User win');
+      } else {
+        alert('Com win');
+      }
+    }
+
+    if (
+      tiles[0].innerText === 'X' &&
+      tiles[3].innerText === 'X' &&
+      tiles[6].innerText === 'X' 
+      ) {
+      if (tiles[0].innerText === 'X') {
+        alert('User win');
+      } else {
+        alert('Com win');
+      }
+    }
+
+    if (
+      tiles[1].innerText === 'X' &&
+      tiles[4].innerText === 'X' &&
+      tiles[7].innerText === 'X'
+      ) {
+      if (tiles[1].innerText === 'X') {
+        alert('User win');
+      } else {
+        alert('Com win');
+      }
+    }
+
+    if (
+      tiles[2].innerText === 'X' &&
+      tiles[5].innerText === 'X' &&
+      tiles[8].innerText === 'X'
+      ) {
+      if (tiles[2].innerText === 'X') {
+        alert('User win');
+      } else {
+        alert('Com win');
+      }
+    }
+
+    if (
+     tiles[0].innerText === 'X' &&
+     tiles[4].innerText === 'X' &&
+     tiles[8].innerText === 'X') {
+      if (tiles[0].innerText === 'X') {
+        alert('User win');
+      } else {
+        alert('Com win');
+      }
+    }
+
+    if (
+      tiles[2].innerText === 'X' &&
+      tiles[4].innerText === 'X' &&
+      tiles[6].innerText === 'X'
+      ) {
+      if (tiles[2].innerText === 'X') {
+        alert('User win');
+      } else {
+        alert('Com win');
+      }
+    }
+  }
+  
   const userMark = (tile, index) => {
     tile.innerText = 'X';
+
+    // check winner
+    checkWinner(tiles)
 
     websocket = new WebSocket("ws://localhost:8080/mark")
 
@@ -12,6 +112,9 @@ window.addEventListener('DOMContentLoaded', () => {
       idxServerMark = event.data;
       console.log("websocket: server choices tile number ", idxServerMark)
       tiles[idxServerMark].innerText = 'O'
+
+      // check winner
+      checkWinner(tiles)
     }
   }
 
